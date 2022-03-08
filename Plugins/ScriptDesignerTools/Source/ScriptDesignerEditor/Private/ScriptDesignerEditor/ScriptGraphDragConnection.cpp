8cd7ee71eea006aaa4f1345305fa895414646443
koramga <koramga@gmail.com>
2022-03-08 22:07:07 +0900

    ScriptGraphDragConnection Working on..

--- Plugins/ScriptDesignerTools/Source/ScriptDesignerEditor/Private/ScriptDesignerEditor/ScriptGraphDragConnection.cpp
+++ Plugins/ScriptDesignerTools/Source/ScriptDesignerEditor/Private/ScriptDesignerEditor/ScriptGraphDragConnection.cpp
@@ -2,3 +2,58 @@
 
 
 #include "ScriptDesignerEditor/ScriptGraphDragConnection.h"
+#include "Widgets/SBoxPanel.h"
+#include "Framework/Application/SlateApplication.h"
+#include "Widgets/Images/SImage.h"
+#include "EdGraph/EdGraph.h"
+#include "SGraphPanel.h"
+#include "ScopedTransaction.h"
+
+TSharedRef<FScriptGraphDragConnection> FScriptGraphDragConnection::New(const TSharedRef<SGraphPanel>& InGraphPanel,
+	const FDraggedPinTable& InStartingPins)
+{
+	TSharedRef<FScriptGraphDragConnection> Operation = MakeShareable(new FScriptGraphDragConnection(InGraphPanel, InStartingPins));
+	Operation->Construct();
+	
+	return Operation;
+}
+
+void FScriptGraphDragConnection::OnDrop(bool bDropWasHandled, const FPointerEvent& MouseEvent)
+{
+	FGraphEditorDragDropAction::OnDrop(bDropWasHandled, MouseEvent);
+}
+
+void FScriptGraphDragConnection::HoverTargetChanged()
+{
+	FGraphEditorDragDropAction::HoverTargetChanged();
+}
+
+FReply FScriptGraphDragConnection::DroppedOnPin(FVector2D ScreenPosition, FVector2D GraphPosition)
+{
+	return FGraphEditorDragDropAction::DroppedOnPin(ScreenPosition, GraphPosition);
+}
+
+FReply FScriptGraphDragConnection::DroppedOnNode(FVector2D ScreenPosition, FVector2D GraphPosition)
+{
+	return FGraphEditorDragDropAction::DroppedOnNode(ScreenPosition, GraphPosition);
+}
+
+FReply FScriptGraphDragConnection::DroppedOnPanel(const TSharedRef<SWidget>& Panel, FVector2D ScreenPosition,
+	FVector2D GraphPosition, UEdGraph& Graph)
+{
+	return FGraphEditorDragDropAction::DroppedOnPanel(Panel, ScreenPosition, GraphPosition, Graph);
+}
+
+void FScriptGraphDragConnection::OnDragged(const FDragDropEvent& DragDropEvent)
+{
+	FGraphEditorDragDropAction::OnDragged(DragDropEvent);
+}
+
+void FScriptGraphDragConnection::ValidateGraphPinList(TArray<UEdGraphPin*>& OutValidPins)
+{
+}
+
+FScriptGraphDragConnection::FScriptGraphDragConnection(const TSharedRef<SGraphPanel>& GraphPanel,
+                                                       const FDraggedPinTable& DraggedPins)
+{
+}
--- Plugins/ScriptDesignerTools/Source/ScriptDesignerEditor/Public/ScriptDesignerEditor/ScriptGraphDragConnection.h
+++ Plugins/ScriptDesignerTools/Source/ScriptDesignerEditor/Public/ScriptDesignerEditor/ScriptGraphDragConnection.h
@@ -12,6 +12,8 @@
 /**
  * 
  */
+class SGraphPanel;
+class UEdGraph;
 
 class FScriptGraphDragConnection : public FGraphEditorDragDropAction
 {
@@ -20,6 +22,35 @@ public :
 
 	typedef TArray<FGraphPinHandle> FDraggedPinTable;
 	static TSharedRef<FScriptGraphDragConnection> New(const TSharedRef<SGraphPanel>& InGraphPanel, const FDraggedPinTable& InStartingPins);
-	
+
+	//FDragDropOperation Interface
+	virtual void OnDrop(bool bDropWasHandled, const FPointerEvent& MouseEvent) override;
+	//End of FDragDropOperaiton Interface
+
+	//FGraphEditorDragDrop Action Interface
+	virtual void HoverTargetChanged() override;
+	virtual FReply DroppedOnPin(FVector2D ScreenPosition, FVector2D GraphPosition) override;
+	virtual FReply DroppedOnNode(FVector2D ScreenPosition, FVector2D GraphPosition) override;
+	virtual FReply DroppedOnPanel(const TSharedRef<SWidget>& Panel, FVector2D ScreenPosition, FVector2D GraphPosition, UEdGraph& Graph) override;
+	virtual void OnDragged(const FDragDropEvent& DragDropEvent) override;
+	//End of FGraphEditorDragDrop Action Interface
+
+	/*
+	 *	Function to check validity of graph pins in the StartPins list. This check helps to prevent processing graph pins which are outdated. 
+	 */
+	virtual void ValidateGraphPinList(TArray<UEdGraphPin*>& OutValidPins);
+
+protected:
+	typedef FGraphEditorDragDropAction Super;
+
+	//Constructor : Make sure to call Construct() after factorying one of these.
+	FScriptGraphDragConnection(const TSharedRef<SGraphPanel>& GraphPanel, const FDraggedPinTable& DraggedPins);
+
+protected:
+	TSharedRef<SGraphPanel> GraphPanel;
+	FDraggedPinTable DraggingPins;
+
+	/* Offset information for the decorator widget */
+	FVector2D DecoratorAdjust;
 	
 };
